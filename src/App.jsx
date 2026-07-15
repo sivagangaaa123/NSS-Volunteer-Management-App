04317dc2eecf8e72c1857f3afb61f14363d52c76
sivagangaaa123 <sivagangaaa123@gmail.com>
2026-07-15 06:34:59 +0530

    NSS Volunteer App nav/view Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddVolunteer from './components/AddVolunteer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ViewAllVolunteers from './components/ViewAllVolunteers'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <AddVolunteer/>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<AddVolunteer/>} />
+      <Route path='/view' element={<ViewAllVolunteers/>} /> 
+     </Routes>
+     </BrowserRouter>
      </>
   )
 }
